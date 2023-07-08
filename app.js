c01575e72658251251fb88430fb09c69672557c0
vkunreal <75563669+vkunreal@users.noreply.github.com>
2023-07-08 15:17:40 +0300

    Init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: true })
+})
+
+app.listen(5000, () => {
+  console.log('Server started in port ' + 5000)
+})
